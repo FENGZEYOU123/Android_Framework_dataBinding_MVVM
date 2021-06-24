afb1b606bc54668dc98e027da7d2a6330f1e81d5
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-06-24 16:44:55 +0800

    debug

--- project/app/src/main/java/com/yfz/mvvm_databinding/twoWayBind/ViewModel.java
+++ project/app/src/main/java/com/yfz/mvvm_databinding/twoWayBind/ViewModel.java
@@ -3,8 +3,18 @@ package com.yfz.mvvm_databinding.twoWayBind;
 import androidx.databinding.ObservableField;
 
 public class ViewModel {
+
    public ObservableField<String> editTextInput = new ObservableField();
    public ObservableField<String> getEditTextInput(){
        return editTextInput;
     }
+
+   public String isChecked ="checkBox已经被选中";
+   public String isUnChecked ="未被选中";
+   public ObservableField<Boolean> checkBoxStatus = new ObservableField<>();
+   public ObservableField<Boolean> getCheckBoxStatus(){
+       return checkBoxStatus;
+   }
+
+
 }
