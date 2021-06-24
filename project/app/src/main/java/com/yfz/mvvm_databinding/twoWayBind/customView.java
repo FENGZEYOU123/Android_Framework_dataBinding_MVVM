b1923f68c7de66bf3d38832c7cd01d17ccea3a51
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-06-24 15:33:32 +0800

    debug

---
+++ project/app/src/main/java/com/yfz/mvvm_databinding/twoWayBind/customView.java
@@ -0,0 +1,13 @@
+package com.yfz.mvvm_databinding.twoWayBind;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class customView extends View {
+    public customView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
